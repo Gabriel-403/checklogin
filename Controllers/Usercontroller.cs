ed525b4154d72ddaa5b80ee6d3e63f2340b911c6
chaos <1499015923@qq.com>
2020-04-23 17:18:16 +0800

    添加项目文件。

---
+++ Controllers/Usercontroller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace login.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("checkLogin")] // checkLogin?id=3
+        public IActionResult Login([FromQuery] int id)
+        {
+
+
+            return Ok(new { code = 0 });
+        }
+
+        [HttpPost("checkLogin")]
+        public IActionResult Login(string Name, string Password)
+        {
+
+            Console.WriteLine(Name);
+            return Ok(new { code = 0 });
+        }
+    }
+}
